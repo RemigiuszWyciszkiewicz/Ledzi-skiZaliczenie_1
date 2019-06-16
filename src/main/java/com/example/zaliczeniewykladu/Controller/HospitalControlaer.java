39bc8e6c626d8240940f06fdb75355e67413e0b8
RemigiuszWyciszkiewicz <remigiuszwyciszkiewicz000@gmail.com>
2019-06-16 14:01:24 +0200

    przerwa na obiad

---
+++ src/main/java/com/example/zaliczeniewykladu/Controller/HospitalControlaer.java
@@ -0,0 +1,14 @@
+package com.example.zaliczeniewykladu.Controller;
+
+import com.example.zaliczeniewykladu.Service.HospitalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HospitalControlaer {
+
+    @Autowired
+    HospitalService hospitalService;
+
+    
+}
