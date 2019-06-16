a660e105e8387107316a61e0ccc292feef4c50e0
RemigiuszWyciszkiewicz <remigiuszwyciszkiewicz000@gmail.com>
2019-06-16 13:59:59 +0200

    controller & service

--- src/main/java/com/example/zaliczeniewykladu/Repository/VisitRepository.java
+++ src/main/java/com/example/zaliczeniewykladu/Repository/VisitRepository.java
@@ -2,6 +2,9 @@ package com.example.zaliczeniewykladu.Repository;
 
 import com.example.zaliczeniewykladu.Dao.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface VisitRepository extends JpaRepository<Visit,Long> {
 }
