14ecc977f7598485f39bc5ed297121b78be1322e
RemigiuszWyciszkiewicz <remigiuszwyciszkiewicz000@gmail.com>
2019-06-17 14:49:10 +0200

    update

---
+++ src/main/java/com/example/zaliczeniewykladu/Controller/InputFromUser.java
@@ -0,0 +1,12 @@
+package com.example.zaliczeniewykladu.Controller;
+
+import lombok.Data;
+
+@Data
+public class InputFromUser {
+
+     String imieFryzjera;
+     String nazwiskoFryzjera;
+     int dzien;
+     
+}
